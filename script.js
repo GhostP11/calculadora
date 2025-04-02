db40be0a7dac5d4717d676997d6e3079b5d2642c
GhostP11 <emmanuelpr10@gmail.com>
2025-04-02 01:33:51 -0600

    Restructuración de la calculadora para evitar utilizar el metodo Eval() de JS, optimizando rendimiento y reduciendo riesgos dde inyección de código

--- script.js
+++ script.js
@@ -1,30 +1,103 @@
-const display = document.querySelector("#display")
+let num1 = 'noNumber';
+let num2 = 'noNumber';
+let displayNumber = 0;
+let buttonValue = 0;
+let currentValue = 0;
+let mathFunction = '';
+let answere = 0;
+let clearDisplay = -1;
+let res = 0;
+let displayValue = 0;
+let includesDecimal = '';
+
+function add(num1, num2) {
+    res = num1 + num2;
+}
+
+function subtract(num1, num2) {
+    res = num1 - num2;
+}
+
+function multiply(num1, num2) {
+    res = num1 * num2;
+}
+
+function divide(num1,num2) {
+    res = num1 / num2;
+}
+
+function clearTotal() {
+    res === 0;
+}
+
+function operate(num1, num2, mathFunction) {
+    if (mathFunction === "add") {
+        add(num1, num2);
+    } else if (mathFunction === 'subtract') {
+        subtract(num1, num2);
+    } else if (mathFunction === 'multiply') {
+        multiply(num1, num2);
+    } else if (mathFunction === 'divide') {
+        divide(num1, num2);
+    } else if (mathFunction === 'divide' && num2 === 0) {
+        displayElement.textContent = displayNumber;
+        alert('Error quieres dividir entre 0');
+    } else if (mathFunction === 'clearTotal') {
+        clearTotal();
+    } else {
+    }
+}
+
+function concatNumbers() {
+    displayElement = document.getElementById('displayScreen');
+    displayNumber = displayElement.textContent;
+    buttonValue = this.textContent;
+    decimal.disabled = false;
+    desimalCheck
+    if (displayNumber === '0') {
+        displayElement.textContent = buttonValue;
+        currentValue = Number(displayNumber + buttonValue);
+    } else if (clearDisplay === 1 && displayNumber.length < 9) {
+        displayNumber = '';
+        currentValue = 0;
+        clearDisplay = -1;
+        displayElement.textContent = displayNumber + buttonValue;
+        currentValue = Number(displayNumber + buttonValue);
+    } else if (displayNumber.length < 9) {
+        displayElement.textContent = displayNumber + buttonValue;
+        currentValue = Number(displayNumber + buttonValue);
+    } else {
+    }
+}
+
+const numberButtons = document.querySelector('.numbers');
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', concatNumbers);
+});
+
+function clearDisplayFunction() {
+    if (clearDisplay === 1) {
+        clearDisplay = -1;
+    } else {
+        ''
+    }
+}
+
+const clear = document.querySelector('#clear');
+
+clear.addEventListener('click', () => {
+    displayElement.textContent = 0;
+    num1 = 'noNumber';
+    num2 = 'noNumber';
+});
+
+const equals = document.querySelector('#equals');
 const buttons = document.querySelectorAll("button")
-const calculator = document.querySelector(".calculator");
 const toggle = document.querySelector(".toggle")
 
-buttons.forEach((item)=>{
-    item.onclick=()=>{
-        if(item.id=="clear"){
-            display.innerText="";
-        }else if(item.id=="backspace"){
-            let string = display.innerText.toString();
-            display.innerText=string.substr(0,string.length-1);
-        }else if(display.innerText !="" && item.id=="equal"){
-            display.innerText = eval(display.innerText);
-        }else if(display.innerText == "" && item.id == "equal") {
-            display.innerText = "Null";
-            setTimeout(()=>(display.innerText=""), 2000);
-        }else{
-            display.innerText+=item.id;
-        }
-    }
-})
-
-let isDark = true;
-
-toggle.onclick=()=>{
+toggle.onclick = () => {
     toggle.classList.toggle("active");
     calculator.classList.toggle("dark")
-    isDark=!isDark
+    isDark = !isDark
 }
